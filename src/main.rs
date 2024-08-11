bfecd91f707034b11c4a274c888672b3449df3b2
Paras <161898004+paradoxedmind@users.noreply.github.com>
2024-08-11 19:22:36 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,20 @@
+use anyhow::Result;
 use std::env;
 use std::io;
 use std::process;
 
 fn match_pattern(input_line: &str, pattern: &str) -> bool {
-    if pattern.chars().count() == 1 {
-        return input_line.contains(pattern);
-    } else {
-        panic!("Unhandled pattern: {}", pattern)
+    match pattern.chars().count() {
+        2 => match pattern {
+            "\\d" => input_line.chars().any(|c| c.is_digit(10)),
+            _ => panic!("Unrecognized Pattern: {pattern}"),
+        },
+        _ => return input_line.contains(pattern),
     }
 }
 
 // Usage: echo <input_text> | your_program.sh -E <pattern>
-fn main() {
+fn main() -> Result<()> {
     if env::args().nth(1).unwrap() != "-E" {
         println!("Expected first argument to be '-E'");
         process::exit(1);
@@ -22,7 +25,6 @@ fn main() {
 
     io::stdin().read_line(&mut input_line).unwrap();
 
-    // Uncomment this block to pass the first stage
     if match_pattern(&input_line, &pattern) {
         process::exit(0)
     } else {
