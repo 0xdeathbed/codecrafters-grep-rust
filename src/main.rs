43f8f50808cabead458c6cf8a9c70b25339b6094
Paras <161898004+paradoxedmind@users.noreply.github.com>
2024-08-11 19:36:40 +0530

    Positive character Groups

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,20 @@ use std::io;
 use std::process;
 
 fn match_pattern(input_line: &str, pattern: &str) -> bool {
-    match pattern.chars().count() {
-        2 => match pattern {
-            "\\d" => input_line.chars().any(|c| c.is_digit(10)),
-            "\\w" => input_line
-                .chars()
-                .any(|c| c.is_ascii_alphanumeric() || c == '_'),
-            _ => panic!("Unrecognized Pattern: {pattern}"),
-        },
-        _ => return input_line.contains(pattern),
+    match pattern {
+        "" => panic!("Empty Pattern not allowed"),
+        "\\d" => input_line.chars().any(|c| c.is_digit(10)),
+        "\\w" => input_line
+            .chars()
+            .any(|c| c.is_ascii_alphanumeric() || c == '_'),
+        positve
+            if positve.chars().nth(0).is_some_and(|c| c == '[')
+                && positve.chars().last().is_some_and(|c| c == ']') =>
+        {
+            let p = &positve[1..positve.len()];
+            input_line.chars().any(|c| p.contains(c))
+        }
+        _ => input_line.contains(pattern),
     }
 }
 
